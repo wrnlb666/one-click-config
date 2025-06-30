10383be74a72fdaec811d1b45266cd1e8522df5a
wrnlb666 <103810793+wrnlb666@users.noreply.github.com>
2025-06-30 23:42:33 +1000

    fix bug for starship config

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ itmux() {
 istarship() {
     info starship
     git clone git@github.com:wrnlb666/starship.toml.git starship
-    ln -sf $(pwd)/starship/starship.toml ~/
+    ln -sf $(pwd)/starship/starship.toml ~/.config/
 }
 
 # gdb
