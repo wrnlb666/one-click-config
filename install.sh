6d2ae58d6c2628ce94d263d545558998962733de
wrnlb666 <wrnlb666@gmail.com>
2025-09-09 00:44:21 +1000

    use swd instead of pwd

--- install.sh
+++ install.sh
@@ -12,11 +12,34 @@ if [[ ! -x "$(command -v yq)" ]]; then
     fi
 fi
 
+# Script Working Directory
+swd()
+{
+    local SOURCE_PATH="${BASH_SOURCE[0]}"
+    local SYMLINK_DIR
+    local SCRIPT_DIR
+    # Resolve symlinks recursively
+    while [ -L "$SOURCE_PATH" ]; do
+        # Get symlink directory
+        SYMLINK_DIR="$( cd -P "$( dirname "$SOURCE_PATH" )" >/dev/null 2>&1 && pwd )"
+        # Resolve symlink target (relative or absolute)
+        SOURCE_PATH="$(readlink "$SOURCE_PATH")"
+        # Check if candidate path is relative or absolute
+        if [[ $SOURCE_PATH != /* ]]; then
+            # Candidate path is relative, resolve to full path
+            SOURCE_PATH=$SYMLINK_DIR/$SOURCE_PATH
+        fi
+    done
+    # Get final script directory path from fully resolved source path
+    SCRIPT_DIR="$(cd -P "$( dirname "$SOURCE_PATH" )" >/dev/null 2>&1 && pwd)"
+    echo "$SCRIPT_DIR"
+}
+
 
 # Global Variables
 root="$(pwd)"
 dir="${HOME}/wrnlb/config"
-config=$(cat "${root}/config.yaml")
+config=$(cat "$(swd)/config.yaml")
 ins_all=false
 mapfile -t keys < <(echo "$config" | yq 'keys[]')
 
