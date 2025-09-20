b224b91b5d1cba233f23dd4c80c70d89e11ee3f6
wrnlb666 <wrnlb666@gmail.com>
2025-09-20 20:15:10 +1000

    add util to download yq directly from github

--- util.sh
+++ util.sh
@@ -1,5 +1,50 @@
 
 # Try installing mikefarah/yq if not exists
+_install_yq_binary() {
+    local err="[ERRO] please install yq manually"
+    local required=("uname" "curl")
+    for r in ${required[@]}; do
+        if [[ ! -x "$(command -v "$r")" ]]; then
+            echo "$err"
+            return 1
+        fi
+    done
+    # check OS
+    local OS="$(uname -s)"
+    local os
+    case "$OS" in
+        Linux)
+            os="linux"
+            ;;
+        *)
+            echo "$err"
+            return 1
+            ;;
+    esac
+    # check ARCH
+    local ARCH="$(uname -m)"
+    local arch
+    case "$ARCH" in
+        x86_64)
+            arch="amd64"
+            ;;
+        *)
+            echo "$err"
+            return 1
+            ;;
+    esac
+    # install yq from github
+    [[ -d ~/.local/bin ]] || mkdir -p ~/.local/bin
+    curl "https://github.com/mikefarah/yq/releases/latest/download/yq_${os}_${arch}" \
+        -Lo ~/.local/bin/yq
+    if [[ $? -ne 0 ]]; then
+        echo "$err"
+        return 1
+    fi
+    chmod +x ~/.local/bin/yq
+    export PATH="${HOME}/.local/bin:$PATH"
+}
+
 _install_yq() {
     if [[ ! -x "$(command -v yq)" ]]; then
         echo "[WARN] yq not installed"
@@ -16,8 +61,7 @@ _install_yq() {
         elif [[ -x "$(command -v snap)" ]]; then
             snap install yq
         else
-            echo "[ERRO] please install yq manually"
-            exit 1
+            _install_yq_binary || exit 1
         fi
     fi
 }
